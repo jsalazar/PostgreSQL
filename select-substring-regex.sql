1d7a8b9796cf2c6bdcf3f84b928741973241fd9e
Jake Salazar <jake.salazar@gmail.com>
2014-02-13 13:39:23 -0600

    Create select-substring-regex.sql

---
+++ select-substring-regex.sql
@@ -0,0 +1,14 @@
+select substring(click_fancydate, '[a-zA-Z0-9:. ]{1,16}') 
+from click_tracker 
+where click_linkid = '123456489';
+
+/*
+
+FUNCTION
+SELECT SUBSTRING('XY1234Z', 'Y*([0-9]{1,3})');
+Result: 123
+
+DESCRIPTION
+regular expression matching rules
+
+*/
