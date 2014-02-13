5ea0891aa9a5506eeb06e921f3d084eb4727e86b
Jake Salazar <jake.salazar@gmail.com>
2014-02-13 00:13:19 -0600

    Update postgres-extract-group-order.sql

--- postgres-extract-group-order.sql
+++ postgres-extract-group-order.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT click_linkid, count (*) as count, click_campaign, click_linkurl
         FROM click_tracker
         where extract(YEAR from TIMESTAMP WITH TIME ZONE 'epoch' + click_timestamp * INTERVAL '1 second') = 2009        
         GROUP BY click_linkid, click_campaign, click_linkurl 
-        ORDER BY click_campaign, count DESC ";    
+        ORDER BY click_campaign, count DESC;
