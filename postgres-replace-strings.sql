ac6b82c22c9fb3ee395371b8b001a9dbbdbfc753
Jake Salazar <jake.salazar@gmail.com>
2014-02-13 13:19:22 -0600

    Update postgres-replace-strings.sql

--- postgres-replace-strings.sql
+++ postgres-replace-strings.sql
@@ -1,3 +1,12 @@
 select * 
 from volunteer_registrations 
 where REPLACE(REPLACE(volunteer_site, '''',''), '\\\','') = '$siteName';
+
+/*
+FUNCTION
+replace(string text, from text, to text)
+
+DESCRIPTION
+Replace all occurrences in string of substring from with substring to
+
+*/
