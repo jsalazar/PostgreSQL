b694469a017148f7cd79b434def91f6dca40461b
Jake Salazar <jake.salazar@gmail.com>
2014-02-13 13:43:52 -0600

    Update select-substring-regex.sql

--- select-substring-regex.sql
+++ select-substring-regex.sql
@@ -2,6 +2,13 @@ select substring(click_fancydate, '[a-zA-Z0-9:. ]{1,16}')
 from click_tracker 
 where click_linkid = '123456489';
 
+
+// similar
+
+select distinct substr(click_fancydate, 1, 16) as uniquedayclick 
+from click_tracker 
+where click_linkid = '123456489';
+
 /*
 
 FUNCTION
